c165b1e28f916e475b57bc79aa319044c1478912
DMyrzakanov <Danntu1986#>
2020-01-11 21:23:22 +0600

    Constants example added define

--- ConstantExample.php
+++ ConstantExample.php
@@ -1,9 +1,25 @@
 <?php
 
+define('MIN_VALUE', '0.0');   // RIGHT - Works OUTSIDE of a class definition.
+define('MAX_VALUE', '1.0');   // RIGHT - Works OUTSIDE of a class definition.
 
 class ConstantExample
 {
-    const PI = 3.14;
+    const MIN_VALUE = 0;
+    const MAX_VALUE = 1;
+
+    static function getMinValue(){
+        return self::MIN_VALUE;
+    }
+
+
+    static function getMaxValue(){
+        return self::MAX_VALUE;
+    }
 }
 
-echo ConstantExample::PI;
+echo "Min value outside class = ".MIN_VALUE;
+echo "\nMax value outside class = ".MAX_VALUE;
+
+echo "\nMin value in class = ".ConstantExample::getMinValue();
+echo "\nMax value in class = ".ConstantExample::getMaxValue();
