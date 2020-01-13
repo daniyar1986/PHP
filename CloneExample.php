a2836376e97b2b61545223d6147ed6b0b458260b
DMyrzakanov <Danntu1986#>
2020-01-13 19:37:52 +0600

    ООП для начинающих PHP. Магические метод clone

--- CloneExample.php
+++ CloneExample.php
@@ -3,13 +3,51 @@
 
 class CloneExample
 {
+    private $name;
+    private $city;
+    private $id;
+
+    /**
+     * CloneExample constructor.
+     * @param $name
+     * @param $city
+     */
+    public function __construct($name, $city)
+    {
+        $this->name = $name;
+        $this->city = $city;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
     function __clone()
     {
-        echo "Cloned";
+        $this->id = 0;
+    }
+
+    function __get($name)
+    {
+        // TODO: Implement __get() method.
     }
 }
 
-$cloneExample = new CloneExample();
-
+$cloneExample = new CloneExample("Danik","Almaty");
+$cloneExample->setId(133);
+var_dump($cloneExample);
 $obj = clone $cloneExample;
+var_dump($obj);
 
