41eeff6e2db5726dafb362d3c88231ddd67b1b03
DMyrzakanov <Danntu1986#>
2020-01-13 23:03:03 +0600

    Уроки MySQL | Введение. Как подключить базу данных

--- DBConnectionExample.php
+++ DBConnectionExample.php
@@ -25,3 +25,32 @@ try {
 } catch (PDOException $exception){
     echo "Connection failed :".$exception->getMessage();
 }
+
+$sql = "create database testDB";
+
+if ($conn->query($sql) ===TRUE){
+    echo "Database created successfully";
+} else {
+    echo "\nError " . $conn->error;
+}
+
+$sql2 = "create database testDB2";
+
+if (mysqli_query($conn2,$sql2)){
+    echo "Database $sql2 created successfully";
+}  else {
+    echo "\nError " . mysqli_error($conn2);
+}
+mysqli_close($conn2);
+
+
+try {
+    $PDO_conn2 =  new PDO("mysql:host=$server_name; mysql:dbname=newDB;
+    unix_socket=/opt/lampp/var/mysql/mysql.sock",$user_name,$password);
+    echo "\nConnection successfully";
+    $sql3 = "create database testDB3";
+    $PDO_conn2->exec($sql3);
+    echo "\nDatabase $sql3 created successfully";
+} catch (PDOException $exception){
+    echo "Connection failed :".$exception->getMessage();
+}
